1a3ac5fde483c2f7187138f3fbb9013bcdd6c375
Dhanamoorthy <125135677+Dhanamoorthy@users.noreply.github.com>
2023-03-15 19:54:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var request=new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all");
+request.open("GET","https://restcountries.com/v2/all");
 request.send();
 request.onload=function(){
     var result=JSON.parse(request.response);
